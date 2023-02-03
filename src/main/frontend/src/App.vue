f2e3621545823c5d659dfd546bdbb25f5e3a8823
Bryan <bryanpjpgi2b@hotmail.com>
2023-02-03 11:42:18 +0100

    styles for DashboardView finished

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-
+import DashboardView from './views/DashboardView.vue';
 </script>
 
 <template>
   
   <header>
-      
+      <DashboardView/>
     <div class="wrapper">
       
 
---
+++ src/main/frontend/src/components/DashboardComponents/counterAnimals.vue
@@ -0,0 +1,34 @@
+<script>
+</script>
+<template>
+<div class="card mb-3 mt-2"  id="counter">
+  <div class="card-body p-0">
+    <table>
+  <thead>
+    <tr>
+      <th id="title">TOTAL ANIMAL COUNT</th>
+      <th id="number">14</th>
+    </tr>
+  </thead>
+</table>
+  </div>
+</div>
+</template>
+<style lang="scss" scoped>
+#counter{
+    border-color: #D33C0C;
+    border-width: 0.1em;
+}
+#title{
+    font-size: 2em;
+    padding-right: 1em;
+    color: #D33C0C;
+}
+#number{
+    font-size: 2em;
+    padding-left: 1em;
+    padding-right: 1em;
+    background-color: #D33C0C;
+    color: white;
+}
+</style>

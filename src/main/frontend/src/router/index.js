7e2948bac206fecc23711b8b64bcf2aa0af28270
Bryan <bryanpjpgi2b@hotmail.com>
2023-02-09 22:32:18 +0100

    last commit

--- src/main/frontend/src/router/index.js
+++ src/main/frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+// import VueRouter from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -31,5 +32,11 @@ const router = createRouter({
     }
   ]
 })
+
+// const router2 = new VueRouter({
+//   routes: [
+//     {path: '/', redirect:{name: 'dashboard'}}
+//   ]
+// })
 
 export default router
